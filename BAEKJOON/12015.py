2ba9b372ffbde8748678d23678bb8298b63446d1
Adlet <48337548+adlet0331@users.noreply.github.com>
2023-07-04 12:27:40 +0900

    20230704-12015 푸는중

--- BAEKJOON/12015.py
+++ BAEKJOON/12015.py
@@ -1,5 +1,21 @@
 N = int(input())
 data = map(int, input().split())
 
-dp = [[0 for _ in range(N)] for _ in range(N)]
+dp = [0 for _ in range(N)]
 
+def recurse(start):
+    if dp[start] != 0:
+        return dp[start]
+    cnt = 1
+    for i in range(start + 1, N):
+        if data[start] < data[i]:
+            cnt = max(cnt, recurse(i) + 1)
+    dp[start] = cnt
+    return cnt
+
+
+mcnt = 0
+for i in range(N):
+    if dp[i] != 0:
+        continue
+    mcnt = max(mcnt, recurse(i))
