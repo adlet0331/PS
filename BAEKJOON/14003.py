8df4afd0f07b37834eb3837091bb977c379c2024
Adlet <48337548+adlet0331@users.noreply.github.com>
2023-10-17 07:56:23 +0000

    14003 해결 역추적 할 때 시간 초과 문제였음

--- BAEKJOON/14003.py
+++ BAEKJOON/14003.py
@@ -8,37 +8,29 @@ arr = [0] + arr
 dp = [-1000000001]
 trace = [-1 for _ in range(N + 1)]
 
-# bisect_left 로 대체 가능
-# arr[index] 보다 작은, 가장 큰 수가 있는 index
-# def findSmallerMax(index):
-#     num = arr[index]
-#     start = 0
-#     end = len(dp) - 1
-#     while start + 1 < end:
-#         mid = (start + end) // 2
-#         if dp[mid] < num:
-#             start = mid
-#         elif dp[mid] > num:
-#             end = mid
-#         else:
-#             return mid - 1
-#     return start if dp[end] >= num else end
-
 for i in range(1, N + 1):
     mindex = bisect.bisect_left(dp, arr[i]) - 1
     trace[i] = mindex + 1
     if mindex == len(dp) - 1:
         dp.append(arr[i])
-    if dp[mindex + 1] > arr[i]:
+    elif dp[mindex + 1] > arr[i]:
         dp[mindex + 1] = arr[i]
 
 print(len(dp) - 1)
 cnt = len(dp) - 1
-answer = ''
+#answer = ''  이렇게 하니까 시간 초과 남 ㅎㅎ;; 억울하다!
+#for i in range(N, 0, -1):
+#    if trace[i] == cnt:
+#        answer = f'{arr[i]} {answer}'
+#        cnt -= 1
+#    if cnt == 0:
+#        break
+#print(answer)
+answer = []
 for i in range(N, 0, -1):
     if trace[i] == cnt:
-        answer = f'{arr[i]} {answer}'
+        answer.append(arr[i])
         cnt -= 1
-    if cnt == 0:
-        break
-print(answer)
+        if cnt == 0:
+            break
+print(*answer[::-1])
